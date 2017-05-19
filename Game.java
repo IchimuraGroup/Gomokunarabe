3d57e9e96e5aad49d28b56e6a51d18042a7beb09
koutakoduka <c01142144b@edu.teu.ac.jp>
2017-05-19 11:29:59 +0900

    完成

--- Game.java
+++ Game.java
@@ -3,88 +3,95 @@ package com.example.c01142144b.gomokunarabe;
 /**
  * Created by c01142144b on 2017/05/09.
  */
-public class Game{
+public class Game {
     //     Player player;
     private static Player player1 = new Player(Piece.BLACK);
     private static Player player2 = new Player(Piece.WHITE);
     private static Player CurrentPlayer = player1;
+
     //    int PLAYER_COUNT;
     //プレイヤーをセット
-    public static void setPlayer1(Player player){
+    public static void setPlayer1(Player player) {
 //         player = new Player(Piece.BLACK);
         player1 = player;
 //         return player;
 
     }
-    public static void setPlayer2(Player player){
+
+    public static void setPlayer2(Player player) {
 //         player = new Player(Piece.WHITE);
         player2 = player;
 //         return player;
 
     }
+
     //現在のプレイヤーを返す
-    public static Player getCurrentPlayer(){
+    public static Player getCurrentPlayer() {
         return CurrentPlayer;
     }
+
     //プレイヤー交替
-    public static void changeNextPlayer(){
-        if(CurrentPlayer == player1){
+    public static void changeNextPlayer() {
+        if (CurrentPlayer == player1) {
             CurrentPlayer = player2;
-        }else{
+        } else {
             CurrentPlayer = player1;
         }
 
     }
+
     //終了判定➔ゲームの終了
-    public static boolean isFinished(Board board){
+    public static boolean isFinished(Board board) {
         // int[][] board = Board.getCellStatus();
 
 //             boolean finish = false;
         for (int x = 0; x < Board.SIZE; x++) {
             for (int y = 0; y < Board.SIZE; y++) {
                 Cell.STATUS status = board.getCellStatus(x, y);
-                if(status != Cell.STATUS.NONE){
-//                     searchConnectCount(count, status, board, x*dir[X], y*dir[Y], dir);
-                    int count1 = searchConnectCount(0, status, board, x, y, new int[]{0, 1});//上方向
-                    int count2 = searchConnectCount(0, status, board, x, y, new int[]{1, 1});//斜め右上方向
-                    int count3 = searchConnectCount(0, status, board, x, y, new int[]{0, 0});//右方向
-                    int count4 = searchConnectCount(0, status, board, x, y, new int[]{1, -1});//斜め右下方向
-                    int count5 = searchConnectCount(0, status, board, x, y, new int[]{0, -1});//下方向
-                    int count6 = searchConnectCount(0, status, board, x, y, new int[]{-1, -1});//斜め左下方向
-                    int count7 = searchConnectCount(0, status, board, x, y, new int[]{-1, 0});//左方向
-                    int count8 = searchConnectCount(0, status, board, x, y, new int[]{-1, 1});//斜め左上方向
+                if (status != Cell.STATUS.NONE) {
+//                     searchConnectCount(count, status, board, x+dir[X], y+dir[Y], dir);
+//                    int count1 = searchConnectCount(0, status, board, x, y, new int[]{0, 1});//上方向
+//                    int count2 = searchConnectCount(0, status, board, x, y, new int[]{1, 1});//斜め右上方向
+//                    int count3 = searchConnectCount(0, status, board, x, y, new int[]{0, 0});//右方向
+//                    int count4 = searchConnectCount(0, status, board, x, y, new int[]{1, -1});//斜め右下方向
+//                    int count5 = searchConnectCount(0, status, board, x, y, new int[]{0, -1});//下方向
+//                    int count6 = searchConnectCount(0, status, board, x, y, new int[]{-1, -1});//斜め左下方向
+//                    int count7 = searchConnectCount(0, status, board, x, y, new int[]{-1, 0});//左方向
+//                    int count8 = searchConnectCount(0, status, board, x, y, new int[]{-1, 1});//斜め左上方向
+                    int count1 = searchConnectCount(0, status, board, x, y, new int[]{0, 1});//右方向
+                    int count2 = searchConnectCount(0, status, board, x, y, new int[]{1, -1});//斜め右下方向
+                    int count3 = searchConnectCount(0, status, board, x, y, new int[]{0, -1});//下方向
+                    int count4 = searchConnectCount(0, status, board, x, y, new int[]{-1, -1});//斜め左下方向
+                    int count5 = searchConnectCount(0, status, board, x, y, new int[]{-1, 0});//左方向
 
-                    if(count1 >= 5 || count2 >= 5 || count3 >= 5 || count4 >= 5 || count5 >= 5 || count6 >= 5 || count7 >= 5 || count8 >= 5){
-//                          finish = true;
+                    if (count1 >= 5 || count2 >= 5 || count3 >= 5 || count4 >= 5 || count5 >= 5) {
                         return true;
                     }
                 }
             }
         }
 
-          /*  for (int i = 0; i < board.length - 4; i++) {
-                for (int j = 4; j < board.length; j++) {
-                }
-            }*/
         return false;
     }
+
     //指定した座標、方向に連結している同じ種類の駒をカウントし、戻り値として返す
-    private static int searchConnectCount(int count, Cell.STATUS status, Board board, int x, int y, int[] dir){
+    private static int searchConnectCount(int count, Cell.STATUS status, Board board, int x, int y, int[] dir) {
         final int X = 0;
         final int Y = 1;
-        if(x < 0 || x >= Board.SIZE || y < 0 || y >= Board.SIZE){
+        if (x < 0 || x >= Board.SIZE || y < 0 || y >= Board.SIZE) {
             return count;
         }
-
-        if(status == board.getCellStatus(x, y)){
+//      スタックオーバーフローする →　解決
+        if (status == board.getCellStatus(x, y)) {
             count++;
-            count = searchConnectCount(count, status, board, x+dir[X], y+dir[Y], dir);
+            count = searchConnectCount(count, status, board, x + dir[X], y + dir[Y], dir);
         }
 
         return count;
     }
+
     //新しいボードを作る
-    public static Board createNewBoard(){
+    public static Board createNewBoard() {
         return new Board();
     }
 }
